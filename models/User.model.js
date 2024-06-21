2554469b406da636041eaee2f01f0c0f218c5b95
Miguel Chito <96595540+donxito@users.noreply.github.com>
2024-06-21 13:42:01 +0200

    update user model

--- models/User.model.js
+++ models/User.model.js
@@ -35,6 +35,10 @@ const userSchema = new Schema(
     ads: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Ad"
+    },
+    avatar: {
+      type: String,
+      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     }
   },
   {
